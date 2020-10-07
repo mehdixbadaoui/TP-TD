9c31304eed82a50abb0edf54a3777f9f291a856e
mehdi <mehdixbadaoui@outlook.fr>
2020-10-07 20:18:21 +0200

    spawning waves of 5 each 2 seconds

--- Tower Defense/Assets/Scripts/spawner.cs
+++ Tower Defense/Assets/Scripts/spawner.cs
@@ -6,9 +6,25 @@ public class spawner : MonoBehaviour
 {
     public Transform prefab;
     public Transform spawn;
+
+    private float countdown = 2f;
+    public float waverest = 10f;
     // Update is called once per frame
-    void Start()
-    {
-        // Instantiate(prefab, spawn.position, spawn.rotation);
+
+    void Update(){
+        if(countdown <= 0){
+            StartCoroutine(spanWave());
+            Instantiate(prefab, spawn.position, spawn.rotation);
+            countdown = waverest;
+        }   
+        countdown -= Time.deltaTime;
+    }
+
+    IEnumerator spanWave(){
+        for (int i = 0; i < 5; i++)
+        {
+            Instantiate(prefab, spawn.position, spawn.rotation);
+            yield return new WaitForSeconds(0.3f);
+        }
     }
 }
