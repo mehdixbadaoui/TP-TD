7127a615b1469d09fb6991cafaf24c276f7e8778
mehdi <mehdixbadaoui@outlook.fr>
2020-10-09 17:15:13 +0200

    can instantiate turrets in case from grid by clicking

--- Tower Defense/Assets/Scripts/ground_case.cs
+++ Tower Defense/Assets/Scripts/ground_case.cs
@@ -4,10 +4,18 @@ using UnityEngine;
 
 public class ground_case : MonoBehaviour
 {
+    public Transform prefab;
     // Start is called before the first frame update
     void Start()
     {
         
+    }
+
+    void OnMouseDown(){
+
+        Instantiate(prefab, transform.position, transform.rotation);
+        
+
     }
 
     // Update is called once per frame
