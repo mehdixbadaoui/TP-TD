7a3fb523bfc5edc5ccfea44f9ca769ac548f0204
mehdi <mehdixbadaoui@outlook.fr>
2020-10-09 17:44:15 +0200

    cells change colors depending on build ability

--- Tower Defense/Assets/Scripts/ground_case.cs
+++ Tower Defense/Assets/Scripts/ground_case.cs
@@ -5,17 +5,36 @@ using UnityEngine;
 public class ground_case : MonoBehaviour
 {
     public Transform prefab;
+    public bool buildable = true;
+    public Color hover_color_good;
+    public Color hover_color_bad;
+    public Color base_color;
     // Start is called before the first frame update
     void Start()
     {
-        
+        GetComponent<Renderer>().material.color = base_color;
     }
 
+    void OnMouseEnter(){
+        if(buildable){
+            GetComponent<Renderer>().material.color = hover_color_good;
+            
+        }
+        else{
+            GetComponent<Renderer>().material.color = hover_color_bad;
+        }
+    }
     void OnMouseDown(){
+        if(buildable){
+            Instantiate(prefab, transform.position, transform.rotation);
 
-        Instantiate(prefab, transform.position, transform.rotation);
+        }
         
 
+    }
+
+    void OnMouseExit(){
+        GetComponent<Renderer>().material.color = base_color;
     }
 
     // Update is called once per frame
